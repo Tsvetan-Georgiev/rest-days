37742117b01c4b663c3367a3fa97e3c355b69d70
moxata <georgi.stefanov@gmail.com>
2016-07-28 19:19:58 +0300

    formatting + better leap year algorithm

--- dni.php
+++ dni.php
@@ -12,61 +12,41 @@ function restDaysYear($getyear){
 		$RB = 2*$R2 + 4*$R3 + 6*$R4;
 		$R5 = $RB % 7;
 		$RC = $R4 + $R5;
-			if ($RC<28){
-				$RC=$RC+3;
-				$m=4;
-			}
-			else if($RC>=28){
-				$RC=$RC-27;
-			    $m=5;
-			}
-			// ако великден е май или денят е преди 10-ти слага нула на деня
-			if ($m==5 or $RC<10) {
+
+		if ($RC<28){
+			$RC=$RC+3;
+			$m=4;
+		}
+		else if($RC>=28){
+			$RC=$RC-27;
+		    $m=5;
+		}
+
+		// ако великден е май или денят е преди 10-ти слага нула на деня
+		if ($m==5 or $RC<10) {
 			// стринг от точната дата на великден
-				$Razppetak="0".$RC."-"."0".$m."-".$G;
+			$Razppetak="0".$RC."-"."0".$m."-".$G;
 			// записва датата в секунди по линукският формат
-				$Razppetak=strtotime($Razppetak);
+			$Razppetak=strtotime($Razppetak);
 			// добавя или изважда дни според втория зададен параметър на функцията
-				$Razppetak2=strtotime(date("d-m-Y",$Razppetak)."-"."$petilipon"." day");
+			$Razppetak2=strtotime(date("d-m-Y",$Razppetak)."-"."$petilipon"." day");
 			// превръща датата във формат МесецДен-Година за да е по-лесно да бъде сортиран с останалите дати
-				$Razppetak2=date("Y-m-d",$Razppetak2);
-			}
-			else{
-				$Razppetak=$RC."-"."0".$m."-".$G;
-				$Razppetak=strtotime($Razppetak);
-				$Razppetak2=strtotime(date("d-m-Y",$Razppetak)."-"."$petilipon"." day");
-				$Razppetak2=date("Y-m-d",$Razppetak2);
-			}
+			$Razppetak2=date("Y-m-d",$Razppetak2);
+		}
+		else{
+			$Razppetak=$RC."-"."0".$m."-".$G;
+			$Razppetak=strtotime($Razppetak);
+			$Razppetak2=strtotime(date("d-m-Y",$Razppetak)."-"."$petilipon"." day");
+			$Razppetak2=date("Y-m-d",$Razppetak2);
+		}
+		
 		return $Razppetak2;
 	}
 
-	/**
-	 *  Функция връщаща броя на дните във февруари
-	 *
-	 *  @param int $G Година
-	 *
-	 *	@return int Брой дни
-	 */
-	function getDaysFev($G){
-		$RC= date("L",strtotime("$G"."-02-02"));
-		if ($RC==1) {
-			$RC=29;
-			return $RC;
-		} else {
-			$RC=28;
-			return $RC;
-		}
-	}
-
 	// високосна ли е годината
-	function leap_year(){
-		global $getyear;
-		if(getDaysFev($getyear)==29){
-			return 266;
-		}
-		else{
-			return 265;
-		}
+	function isLeapYear($year)
+	{
+		return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year %400) == 0)));
 	}
 
 	//Намиране на съботите и неделите в дадена година
@@ -98,14 +78,11 @@ function restDaysYear($getyear){
 	function allinone($getyear){
 		global $hdays,$getyear;
 		$all_rest_days=0;
-		$leap_year=leap_year();
+		$leap_year=isLeapYear($getyear);
 		ksort($hdays);
 	}
 
 	getWeekendsOfYear($getyear);
-	$fevruari_days=getDaysFev($getyear);
-	//$Razppetak=velikDen($getyear,"2");
-	//$Ponedlk=velikDen($getyear,"-1");
 	$hdays[velikDen($getyear,"2")] = "Великден";
 	$hdays[velikDen($getyear,"1")] = "Великден";
 	$hdays[velikDen($getyear,"0")] = "Великден";
