3dd184bf4f8dc52114ae363b5bd773746a8a3388
georgiev_h_tsvetan@yahoo.com <Smorkov_01>
2016-07-29 13:28:33 +0300

    print hdays in table

--- print-dni.php
+++ print-dni.php
@@ -1,7 +1,65 @@
 <meta charset="UTF-8">
+<style>
+    table{
+        border: 1px solid black;
+    }
+    td{
+        padding: 1px;
+        border-left: 1px solid black;
+    }
+    td:first-child{
+        border-left: none;
+    }
+    thead td{
+        font-weight: bold;
+        border: none;
+    }
+</style>
+    <table>
+        <thead>
+            <tr>
+                <td>
+                    Unixtime
+                </td>
+                <td>
+                    Дата
+                </td>
+                <td>
+                     Наименование
+                </td>
+            </tr>
+        </thead>
+
 <?php
-	// проба 3
+
 	include "dni.php";
 	$check = restDaysYear("2015");
-	var_dump($check) ;
+	//var_dump($check) ;
+    $countHDays = 0;
+    foreach ($check as $key => $value) {
+        $countHDays += 1;
+        $unixTime = $key;
+        $name = $value;
+        $date = date("Y-m-d",$key);
+        echo "
+            <tr>
+                <td>
+                    ".$unixTime."
+                </td>
+                <td>
+                    ".$date."
+                </td>
+                <td>
+                    ".$name."
+                </td>
+            </tr>
+        ";
+    }
 ?>
+</table>
+<p>
+    Общо почивни дни:
+    <?php
+        echo $countHDays;
+    ?>
+</p>
