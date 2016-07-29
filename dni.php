5745648b0df00befea5a69eac52e56dfa82678e2
moxata <georgi.stefanov@gmail.com>
2016-07-29 13:28:44 +0300

    dni.php: използване на статични методи на клас и премахване на глобалните променливи

--- dni.php
+++ dni.php
@@ -1,8 +1,58 @@
 <?php
-function restDaysYear($getyear){
-	$hdays[$getyear."-01-01"] = "Нова година";
+/**
+*	Клас съдържащ функции за откриване на почивни дни
+*/
+class RestDays {
 
-	function velikDen($G,$petilipon){
+	/**
+	*	Високосна ли е годината
+	*
+	*	@param int $year Година
+	*	@return bool Дали е високосна година
+	*/
+	private static function isLeapYear($year)
+	{
+		return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year %400) == 0)));
+	}
+
+	/**
+	*	Намиране на съботите и неделите в дадена година
+	*
+	*	@param int $year Година
+	*	@param array &$hdays Масив с почивни дни
+	*	@return void
+	*/
+	private static function getWeekendsOfYear($year, &$hdays){
+		$now ="01.01.".$year;
+		$end_date ="01.01.".($year+1);
+		$now = strtotime($now);
+		$end_date = strtotime($end_date);
+		while (date("Y-m-d", $now) != date("Y-m-d", $end_date)) {
+		    $day_index = date("w", $now);
+		    //проверка дали е събота или неделя
+		    if ($day_index == 0 || $day_index == 6) {
+		    	if ($day_index == 0) {
+		    		$forpush = date("Y-m-d", $now);
+		    		$hdays[$forpush] = "Неделя";
+		    	}
+		    	else{
+		    		$forpush = date("Y-m-d", $now);
+		    		$hdays[$forpush] = "Събота";
+		    	}
+		    }
+		    $now = strtotime(date("Y-m-d", $now) . "+1 day");
+		}
+		return $hdays;
+	}
+
+	/**
+	*	Откриване на дните, когато е Великден
+	*
+	*	@param int $G Година
+	*	@param int $petilipon +/-дни от великден
+	*	@return string Датата на великден +/-$petilipon
+	*/
+	private static function velikDen($G, $petilipon) {
 		$m = 0;
 		$R1 = $G % 19;
 		$R2 = $G % 4;
@@ -39,68 +89,50 @@ function restDaysYear($getyear){
 			$Razppetak2=strtotime(date("d-m-Y",$Razppetak)."-"."$petilipon"." day");
 			$Razppetak2=date("Y-m-d",$Razppetak2);
 		}
-		
+
 		return $Razppetak2;
 	}
 
-	// високосна ли е годината
-	function isLeapYear($year)
-	{
-		return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year %400) == 0)));
-	}
-
-	//Намиране на съботите и неделите в дадена година
-	function getWeekendsOfYear($year){
-		$now ="01.01.".$year;
-		$end_date ="01.01.".($year+1);
-		$now = strtotime($now);
-		$end_date = strtotime($end_date);
-		global $hdays;
-		while (date("Y-m-d", $now) != date("Y-m-d", $end_date)) {
-		    $day_index = date("w", $now);
-		    //проверка дали е събота или неделя
-		    if ($day_index == 0 || $day_index == 6) {
-		    	if ($day_index == 0) {
-		    		$forpush = date("Y-m-d", $now);
-		    		$hdays[$forpush] = "Неделя";
-		    	}
-		    	else{
-		    		$forpush = date("Y-m-d", $now);
-		    		$hdays[$forpush] = "Събота";
-		    	}
-		    }
-		    $now = strtotime(date("Y-m-d", $now) . "+1 day");
-		}
-		return $hdays;
-	}
-
-	// функция за сливане на почивните дни през година с празниците, които не се включват в тях, а са през седмицата
-	function allinone($getyear){
-		global $hdays,$getyear;
-		$all_rest_days=0;
-		$leap_year=isLeapYear($getyear);
+	/**
+	*	Откриване на всички почивни дни в дадена година
+	*
+	*	@param int $year
+	*	@return array Списък с почивни дни
+	*/
+	public static function get($year) {
+		$hdays[$year."-01-01"] = "Нова година";
+		self::getWeekendsOfYear($year, $hdays);
+		$hdays[self::velikDen($year,"2")] = "Великден";
+		$hdays[self::velikDen($year,"1")] = "Великден";
+		$hdays[self::velikDen($year,"0")] = "Великден";
+		$hdays[self::velikDen($year,"-1")] = "Великден";
+		$hdays[$year."-03-03"] = "Национален празник";
+		$hdays[$year."-05-01"] = "Ден на труда";
+		$hdays[$year."-05-06"] = "Гергьовден";
+		$hdays[$year."-05-24"] = "Ден на писмеността";
+		$hdays[$year."-09-06"] = "Ден на съединението";
+		$hdays[$year."-09-22"] = "Независимостта на България";
+		$hdays[$year."-12-24"] = "Коледа";
+		$hdays[$year."-12-25"] = "Коледа";
+		$hdays[$year."-12-26"] = "Коледа";
+		// сортиране по дати
 		ksort($hdays);
-	}
+		foreach ($hdays as $key => $value) {
+			$za_bazata=strtotime($key);
+			$hdaysUnix[$za_bazata] = $value;
+		}
 
-	getWeekendsOfYear($getyear);
-	$hdays[velikDen($getyear,"2")] = "Великден";
-	$hdays[velikDen($getyear,"1")] = "Великден";
-	$hdays[velikDen($getyear,"0")] = "Великден";
-	$hdays[velikDen($getyear,"-1")] = "Великден";
-	$hdays[$getyear."-03-03"] = "Национален празник";
-	$hdays[$getyear."-05-01"] = "Ден на труда";
-	$hdays[$getyear."-05-06"] = "Гергьовден";
-	$hdays[$getyear."-05-24"] = "Ден на писмеността";
-	$hdays[$getyear."-09-06"] = "Ден на съединението";
-	$hdays[$getyear."-09-22"] = "Независимостта на България";
-	$hdays[$getyear."-12-24"] = "Коледа";
-	$hdays[$getyear."-12-25"] = "Коледа";
-	$hdays[$getyear."-12-26"] = "Коледа";
-	foreach ($hdays as $key => $value) {
-		$za_bazata=strtotime($key);
-		$hdaysUnix[$za_bazata] = $value;
+		return $hdaysUnix;
 	}
+}
 
-	return $hdaysUnix;
+/**
+*	Връща масив с всички почивни дни
+*
+*	@param int $year Година
+*	@return array Списък с почивните дни
+*/
+function restDaysYear($year){
+	return RestDays::get($year);
 }
 ?>
