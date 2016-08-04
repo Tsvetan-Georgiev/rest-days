a07c0209c4901c93f3ff6aca2b4165494fd9df47
moxata <georgi.stefanov@gmail.com>
2016-08-04 17:29:38 +0300

    test query

---
+++ api/admin/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+require_once '../connect.php';
+require_once '../dni.php';
+require_once '../import-dni-csv.php';
+
+$year = isset($_REQUEST['year']) ? $_REQUEST['year'] : date("Y");
+
+$conn = connect_db();
+
+if ($stmt = $conn->prepare("SELECT restDay, name from rest_days WHERE restDay>=? and restDay<=?")) {
+
+    $ybeg = "{$year}-01-01";
+    $yend = "{$year}-12-31";
+    $stmt->bind_param("ss", $ybeg, $yend);
+
+    $stmt->execute();
+
+    $stmt->bind_result($restDay, $name);
+
+    while ($stmt->fetch()) {
+        echo "$restDay  $name";
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+
+?>
