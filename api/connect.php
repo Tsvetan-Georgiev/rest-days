504f185f8217e8f27b62c8907e6d6e86a2188da4
moxata <georgi.stefanov@gmail.com>
2016-08-04 16:59:53 +0300

    connect

---
+++ api/connect.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'config.php';
+
+$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+/*
+ * Use this instead of $connect_error if you need to ensure
+ * compatibility with PHP versions prior to 5.2.9 and 5.3.0.
+ */
+if (mysqli_connect_error()) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+
+echo 'Success... ' . $mysqli->host_info . "\n";
+
+$mysqli->close();
+
+?>
