b6e5d3187dcb8497758ffc4053cca8280eb36274
moxata <georgi.stefanov@gmail.com>
2016-08-05 10:20:20 +0300

    initialization of year

--- api/admin/bootstrap.php
+++ api/admin/bootstrap.php
@@ -7,12 +7,17 @@
 
     $year = isset($_REQUEST['year']) ? $_REQUEST['year'] : date("Y");
     $removeDay = isset($_REQUEST['removeDay']) ? $_REQUEST['removeDay'] : null;
+    $again = isset($_REQUEST['again']) ? $_REQUEST['again'] : null;
+    $csv = isset($_REQUEST['csv']) ? $_REQUEST['csv'] : null;
+
+    $ybeg = "{$year}-01-01";
+    $yend = "{$year}-12-31";
 ?>
 <!DOCTYPE html>
 <html lang='bg'>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Bootstrap a month</title>
+        <title>Стартиране на година</title>
         <style>
             table{
                 border: 1px solid black;
@@ -57,20 +62,53 @@ if (!is_null($removeDay)) {
         or trigger_error($conn->error, E_USER_ERROR)) {
 
             $stmt->bind_param("s", $removeDay);
-
             $stmt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+            $stmt->close();
     }
 }
 
-if ($stmt = $conn->prepare("SELECT restDay, name from rest_days WHERE restDay>=? and restDay<=? ORDER BY restDay")
+// стартиране на месеца отначало
+if ($again == 1) {
+    // създаване на списък с почивните дни
+    if (!is_null($csv) && $csv !== '') {
+        $yearCsv = ImportDniCSV::rowToYear($csv);
+        if ($year != $yearCsv) {
+            trigger_error('Invalid year in CSV', E_USER_ERROR);
+        }
+        $hdays = ImportDniCSV::rowToArray($csv);
+    } else {
+        $hdays = null;
+    }
+    $rdo = new RestDays($year, $hdays, (!is_null($hdays)));
+    $hdays = $rdo->get();
+
+    // изтриване на почивните дни за тази година в базата данни
+    if ($stmt = $conn->prepare("DELETE FROM rest_days WHERE restDay>=? AND restDay<=?")
+        or trigger_error($conn->error, E_USER_ERROR)) {
+
+        $stmt->bind_param("ss", $ybeg, $yend);
+        $stmt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+        $stmt->close();
+    }
+
+    // въвеждане почивните дни от масива в базата данни
+    if ($stmt = $conn->prepare("INSERT INTO rest_days(restDay, name) VALUES(?,?)")
+        or trigger_error($conn->error, E_USER_ERROR)) {
+
+        foreach ($hdays as $key => $value) {
+            $stmt->bind_param("ss", $key, $value);
+            $stmt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+        }
+        $stmt->close();
+    }
+}
+
+// печат на таблицата с почивните дни
+if ($stmt = $conn->prepare("SELECT restDay, name from rest_days WHERE restDay>=? AND restDay<=? ORDER BY restDay")
     or trigger_error($conn->error, E_USER_ERROR)) {
 
-    $ybeg = "{$year}-01-01";
-    $yend = "{$year}-12-31";
     $stmt->bind_param("ss", $ybeg, $yend);
-
     $stmt->execute() or trigger_error($stmt->error, E_USER_ERROR);
-
     $stmt->bind_result($restDay, $name);
 
     while ($stmt->fetch()) {
@@ -96,5 +134,13 @@ $conn->close();
 
 ?>
         </table>
+        <h3>Стартиране на година <?= $year ?> <i>отначало</i>:</h3>
+        <form action="bootstrap.php">
+            Csv <i>(optional)</i>:<br>
+            <input type="text" name="csv" size="60"><br><br>
+            <input type="hidden" name="year" value="<?= $year ?>">
+            <input type="hidden" name="again" value="1">
+            <input type="submit" value="Старт!">
+        </form>
     </body>
 </html>
