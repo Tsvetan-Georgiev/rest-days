e941e8eda7f80e1be7fad7a790b1b9a406b9c990
moxata <georgi.stefanov@gmail.com>
2016-08-05 11:34:12 +0300

    fix sql

--- docs/sql/rest_days.sql
+++ docs/sql/rest_days.sql
@@ -7,5 +7,5 @@
 CREATE TABLE IF NOT EXISTS `rest_days` (
   `restDay` date NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`day`)
+  PRIMARY KEY (`restDay`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
