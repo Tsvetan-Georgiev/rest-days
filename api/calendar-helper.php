d7d1c561bfd393f7dc0b63f524c0f8fa97f3d7a6
moxata <georgi.stefanov@gmail.com>
2016-08-05 12:44:06 +0300

    calendar debug

---
+++ api/calendar-helper.php
@@ -0,0 +1,74 @@
+<?php
+require_once 'routines.php';
+
+function printCalendar($year, $month, $hdays) {
+
+    $firstDayOfMonth = strtotime(isoDate($year, $month, 1));
+    $firstDayOfWeek = date('w', $firstDayOfMonth);
+    // нека понеделник е 0
+    $firstDayOfWeek = ($firstDayOfWeek + 6) % 7;
+
+    $lastDayOfMonth = strtotime(date("Y-m-t", $firstDayOfMonth));
+    $lastDayOfWeek = date('w', $lastDayOfMonth);
+    // нека понеделник е 0
+    $lastDayOfWeek = ($lastDayOfWeek + 6) % 7;
+?>
+
+    <table>
+        <thead>
+            <td colspan="7"><?= $month ?></td>
+        </thead>
+        <thead>
+            <td>П</td>
+            <td>В</td>
+            <td>С</td>
+            <td>Ч</td>
+            <td>П</td>
+            <td>С</td>
+            <td>Н</td>
+        </thead>
+        <tr>
+<?php
+    // празни клетки до първия ден на месеца
+    for ($i=0; $i<$firstDayOfWeek; $i++) {
+            echo "<td>&nbsp;</td>";
+    }
+    $currDay = $firstDayOfMonth;
+    $currDayOfWeek = $firstDayOfWeek;
+    $workDays = 0;
+    while ($currDay <= $lastDayOfMonth) {
+
+        $isRest = isset($hdays[date("Y-m-d", $currDay)]);
+
+        $dayI = date('d', $currDay);
+        if ($isRest) {
+            echo "<td class='rest'>{$dayI}</td>";
+        } else {
+            echo "<td>{$dayI}</td>";
+            $workDays++;
+        }
+
+        // неделя - нов ред
+        if ($currDayOfWeek == 6) {
+            echo "</tr>";
+            if ($currDay < $lastDayOfMonth) {
+                echo "<tr>";
+            }
+        }
+
+        $currDay = strtotime("+1 day", $currDay);
+        $currDayOfWeek = ($currDayOfWeek + 1) % 7;
+    }
+    if ($currDayOfWeek != 0) {
+        for ($i=$currDayOfWeek; $i<7; $i++) {
+            echo "<td>&nbsp;</td>";
+        }
+        echo "</tr>";
+    }
+?>
+    </table>
+
+<?php
+    echo "р.д.: {$workDays}";
+}
+?>
---
+++ api/calendar.php
@@ -0,0 +1,72 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+
+    require_once 'connect.php';
+    require_once 'calendar-helper.php';
+
+    $year = isset($_REQUEST['year']) ? $_REQUEST['year'] : date("Y");
+    $ybeg = "{$year}-01-01";
+    $yend = "{$year}-12-31";
+?>
+<!DOCTYPE html>
+<html lang='bg'>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Календар за година <?= $year ?></title>
+        <style>
+            table{
+                border: 1px solid black;
+            }
+            td{
+                padding: 1px;
+                border-left: 1px solid black;
+            }
+            td.rest {
+                background-color: silver;
+            }
+            td:first-child{
+                border-left: none;
+            }
+            thead td{
+                font-weight: bold;
+                border: none;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <table style="border: none">
+            <tr>
+<?php
+$conn = connect_db();
+
+if ($stmt = $conn->prepare("SELECT restDay, name FROM rest_days WHERE restDay>=? AND restDay<=?")
+    or trigger_error($conn->error, E_USER_ERROR)) {
+
+    $stmt->bind_param("ss", $ybeg, $yend);
+    $stmt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+    $stmt->bind_result($restDay, $name);
+
+    $hdays = [];
+    while ($stmt->fetch()) {
+        $hdays[$restDay] = $name;
+    }
+
+    $stmt->close();
+}
+
+for ($mon=1; $mon<=12; $mon++) {
+    echo "<td style='border: none;'>";
+    printCalendar($year, $mon, $hdays);
+    echo "</td>";
+    if ($mon === 6) {
+        echo "</tr><tr>";
+    }
+}
+
+$conn->close();
+?>
+            </tr>
+        </table>
+    </body>
+</html>
